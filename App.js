ec22e1142f1ac5b4192512394afffc621758d7b4
gregory <gregory@monpetitgazon.com>
2022-03-24 22:16:12 +0100

    test app consent

--- App.js
+++ App.js
@@ -22,45 +22,27 @@ import {
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const [consent, setConsent] = useState(false);
+
+  AppConsent.configureWith('a1d95433-3c84-4368-99d9-cd07e744678b', true, true);
 
   useEffect(() => {
     if (consent === false) {
-      AppConsent.configureWith(
-        'a1d95433-3c84-4368-99d9-cd07e744678b',
-        true,
-        true,
-      );
-
-      AppConsent.consentGiven().then(() => {
-        setConsent(true);
-        console.log('CONSENT GIVENT');
-      });
       try {
-        AppConsent.consentAlreadyGiven().then(statusConsent => {
-          console.log('statusConsent', statusConsent);
-          if (statusConsent === true) {
-            setConsent(true);
-          } else {
-            AppConsent.present(false);
-          }
-        });
+        AppConsent.present(false);
       } catch (ex) {}
     }
   }, [consent]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <View>
         <Button
           title="SHOW MODAL CONSENT"
           onPress={() => {
+            AppConsent.consentGiven().then(() => {
+              console.log('CONSENT GIVEN 2');
+            });
             AppConsent.clearConsent();
             AppConsent.present(true);
           }}
@@ -69,24 +51,5 @@ const App = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
